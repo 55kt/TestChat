9e285580e722964a1e6b9b02699aa4d0067bca27
Vlad <boogovo@gmail.com>
2024-06-14 21:44:51 +0300

    Initial Commit

---
+++ TestChat/TestChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestChatApp.swift
+//  TestChat
+//
+//  Created by Vlad on 14/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TestChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
