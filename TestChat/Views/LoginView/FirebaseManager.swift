30be1ea9506c235d541b7e9e5b2c0a73dc0e8977
Vlad <boogovo@gmail.com>
2024-06-18 17:40:16 +0300

    Fix all app is done
    Firestore database works is done

--- TestChat/Views/LoginView/FirebaseManager.swift
+++ TestChat/Views/LoginView/FirebaseManager.swift
@@ -1,1 +1,104 @@
+import SwiftUI
+import Firebase
+import FirebaseStorage
+import FirebaseAuth
+import FirebaseFirestore
 
+//MARK: - Firebase Manager Properties
+class FirebaseManager: NSObject, ObservableObject {
+    
+    let auth: Auth
+    let storage: Storage
+    let firestore: Firestore
+    
+    @Published var isLoginMode = false
+    @Published var email = ""
+    @Published var password = ""
+    @Published var shouldShowImagePicker = false
+    @Published var image: UIImage?
+    @Published var loginStatusMessage = ""
+    
+    static let shared = FirebaseManager()
+    
+    //MARK: - Initializer
+    override init() {
+        FirebaseApp.configure()
+        
+        self.auth = Auth.auth()
+        self.storage = Storage.storage()
+        self.firestore = Firestore.firestore()
+        
+        super.init()
+    }
+    
+    //MARK: - Methods
+    
+    // Login Function
+    func loginUser() {
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password) { result, err in
+            if let err = err {
+                print("Failed to login user:", err)
+                self.loginStatusMessage = "Failed to login user: \(err)"
+                return
+            }
+            print("Successfully logged in as user: \(result?.user.uid ?? "")")
+            self.loginStatusMessage = "Successfully logged in as user: \(result?.user.uid ?? "")"
+        }
+    }
+    
+    // Register Function
+    func createNewAccount() {
+        FirebaseManager.shared.auth.createUser(withEmail: email, password: password) { result, err in
+            if let err = err {
+                print("Failed to create user:", err)
+                self.loginStatusMessage = "Failed to create user: \(err)"
+                return
+            }
+            print("Successfully created user: \(result?.user.uid ?? "")")
+            self.loginStatusMessage = "Successfully created user: \(result?.user.uid ?? "")"
+            self.persistImageToStorage()
+        }
+    }
+    
+    // Image Saving in Firebase Storage
+    func persistImageToStorage() {
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else { return }
+        let ref = FirebaseManager.shared.storage.reference(withPath: uid)
+        guard let imageData = self.image?.jpegData(compressionQuality: 0.5) else { return }
+        ref.putData(imageData, metadata: nil) { metadata, err in
+            if let err = err {
+                self.loginStatusMessage = "Failed to push image to Storage: \(err)"
+                return
+            }
+            ref.downloadURL { url, err in
+                if let err = err {
+                    self.loginStatusMessage = "Failed to retrieve downloadURL: \(err)"
+                    return
+                }
+                self.loginStatusMessage = "Successfully stored image with url: \(url?.absoluteString ?? "")"
+                print(url?.absoluteString)
+                guard let url = url else { return }
+                self.storeUserInformation(imageProfileUrl: url)
+            }
+        }
+    }
+    
+    // Saving User Data Collection in Firestore Database
+    func storeUserInformation(imageProfileUrl: URL) {
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else {
+            print("Failed to get current user uid")
+            return
+        }
+        print("Storing user information for user: \(uid)")
+        let userData = ["email": self.email, "uid": uid, "profileImageUrl": imageProfileUrl.absoluteString]
+        FirebaseManager.shared.firestore.collection("users").document(uid).setData(userData) { err in
+            if let err = err {
+                print("Failed to store user information:", err)
+                self.loginStatusMessage = "Failed to store user information: \(err)"
+                return
+            }
+            print("Successfully stored user information")
+            self.loginStatusMessage = "Successfully stored user information"
+        }
+    }
+}
